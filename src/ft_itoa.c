a020d102a4d00b5584b0587e29e7d72437224966
Noor Schat <nschat@f1r6s5.codam.nl>
2019-10-29 19:47:33 +0100

    feat: main code for ft_itoa

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 18:16:28 by nschat        #+#    #+#                 */
-/*   Updated: 2019/10/29 19:26:49 by nschat        ########   odam.nl         */
+/*   Updated: 2019/10/29 19:47:28 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ int		ft_numlen(int n)
 	int	len;
 
 	len = (n <= 0) ? 1 : 0;
-	n *= (n < 0) ? -1 : 1;
 	while (n)
 	{
 		len++;
@@ -45,7 +44,22 @@ int		ft_numlen(int n)
 char	*ft_itoa(int n)
 {
 	char	*str;
+	int		i;
 
 	str = (char *)str(sizeof(char) * (ft_numlen(n) + 1));
+	i = 0;
+	if (n < 0)
+	{
+		str[i] = '-';
+		n *= -1;
+		i++;
+	}
+	while (n)
+	{
+		str[i] = n % 10 + '0';
+		n /= 10;
+		i++;
+	}
+	str[i] = '\0';
 	return (ft_strrev(str));
 }
