dfe12fa71185562951d1d9b9b4d733bac1a84fce
Noor Schat <nschat@student.codam.nl>
2019-11-03 17:49:49 +0100

    feat: add skeleton bonus funcs and make target

---
+++ src/ft_lstmap.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstmap.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/03 17:36:29 by nschat        #+#    #+#                 */
+/*   Updated: 2019/11/03 17:49:31 by nschat        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	(void)lst;
+	(void)f;
+	(void)del;
+	return (NULL);
+}
