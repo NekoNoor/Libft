5e0fb6b8b0dbf73360995b4cd613bf1da28a2312
Noor Schat <nschat@student.codam.nl>
2019-11-05 15:53:12 +0100

    feat: add long overflow check to atoi

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,11 +6,22 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 17:48:21 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/03 17:18:28 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/05 15:52:30 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_longcheck(long nbr, int sign, char next)
+{
+	if (nbr > 922337203685477580)
+		return ((sign == -1) ? 0 : -1);
+	if (nbr == 922337203685477580 && sign == -1 && next > '8')
+		return (0);
+	if (nbr == 922337203685477580 && sign == 1 && next > '7')
+		return (-1);
+	return (1);
+}
 
 static int	ft_isspace(int c)
 {
@@ -21,8 +32,8 @@ static int	ft_isspace(int c)
 
 int			ft_atoi(const char *str)
 {
-	int	sign;
-	int	nbr;
+	int		sign;
+	long	nbr;
 
 	while (ft_isspace(*str))
 		str++;
@@ -32,8 +43,10 @@ int			ft_atoi(const char *str)
 	nbr = 0;
 	while (ft_isdigit(*str))
 	{
+		if (ft_longcheck(nbr, sign, *str) != 1)
+			return (ft_longcheck(nbr, sign, *str));
 		nbr = nbr * 10 + (*str - '0');
 		str++;
 	}
-	return (nbr * sign);
+	return ((int)nbr * sign);
 }
