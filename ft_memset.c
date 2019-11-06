db94cfe197a6b0657c7c01552260349ae08955f8
Noor Schat <nschat@student.codam.nl>
2019-11-06 18:32:05 +0100

    feat: memset backwards, check for null in substr

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 17:47:27 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/05 22:12:56 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/06 18:19:14 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
-
 	if (b == NULL)
 		return (b);
-	i = 0;
 	while (len)
 	{
-		((unsigned char *)b)[i] = (unsigned char)c;
-		i++;
 		len--;
+		((unsigned char *)b)[len] = (unsigned char)c;
 	}
 	return (b);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 18:16:27 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/06 17:34:48 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/06 18:31:57 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	if (start >= ft_strlen(s))
+	if (s == NULL || start >= ft_strlen(s))
 		return (NULL);
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (substr == NULL)
