9a40cfac42115a49fac3f66df37355b09ef27922
Noor Schat <nschat@student.codam.nl>
2019-11-06 22:02:08 +0100

    feat: strncmp proper cast, use strchr in strtrim

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 17:47:27 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/06 18:19:14 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/06 21:47:58 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	if (b == NULL)
-		return (b);
 	while (len)
 	{
 		len--;
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,11 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/28 18:16:28 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/05 18:21:45 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/06 20:55:04 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_contains(char c, const char *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
 
 char		*ft_strtrim(char const *s1, char const *set)
 {
@@ -33,12 +22,12 @@ char		*ft_strtrim(char const *s1, char const *set)
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	begin = 0;
-	while (s1[begin] && ft_contains(s1[begin], set))
+	while (s1[begin] && ft_strchr(set, s1[begin]))
 		begin++;
 	if (!s1[begin])
 		return (ft_strdup(""));
 	end = ft_strlen(s1) - 1;
-	while (end >= 0 && ft_contains(s1[end], set))
+	while (end >= 0 && ft_strchr(set, s1[end]))
 		end--;
 	len = end - begin + 1;
 	trimmed = (char *)malloc(sizeof(char) * (len + 1));
