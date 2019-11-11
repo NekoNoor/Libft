31235ce780db89fd04f76c4a1adaedf5de7a89d7
Noor Schat <nschat@student.codam.nl>
2019-11-11 10:03:57 +0100

    fix: type in lstadd_front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/06 13:08:37 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/11 09:56:43 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/11 10:03:44 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	if (alist == NULL)
+	if (alst == NULL)
 		return ;
 	if (new != NULL)
 		new->next = *alst;
